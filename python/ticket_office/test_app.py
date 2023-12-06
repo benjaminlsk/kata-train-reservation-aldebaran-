6670315f4b877ef1cbe3dfbf3d4d5ee60bae66e1
M0N0G0N <thibauddandoy3d@gmail.com>
2023-12-06 10:32:24 +0100

    Final push to get up to speed with the fix commit

--- python/ticket_office/test_app.py
+++ python/ticket_office/test_app.py
@@ -1,43 +1,60 @@
+from requests import Session
 import json
-import httpx
 
 
-def test_reserve_seats_from_empty_train():
-    train_id requests.Session000"
-
-    client = httpx.Client()
-    response = client.post(f"http://127.0.0.1:8081/reset/{train_id}")
-    response.raise_for_status()
-
-    response = client.post(
-        "httprequests.Session:8083/reserve", json={"train_id": train_id, "count": 4}
-    )
-
-    assert response.status_code == 200, response.text
-    reservation = response.json()
-    assert reservation["train_id"] == "express_2000"
-    assert len(reservation["seats"]) == 4
-    assert reservation["seats"] == ["1A", "2A", "3A", "4A"]
+from flask import Flask, request
 
 
-requests.Sessiondef test_reserve_four_additional_seats():
-    train_id = "express_2000"
+def create_app():
+    app = Flask("ticket_office")
 
-    session = requests.Session()
-    response = session.post(f"http://127.0.0.1:8081/reset/{train_id}")
-    response.raise_for_status()
+    @app.post("/reserve")
+    def reserve():
+        payload = request.json
+        seat_count = payload["count"]
+        train_id = payload["train_id"]
 
-    response = session.post(
-        "http://127.0.0.1:8083/reserve", json={"train_id": train_id, "count": 4}
-    )
-    assert response.status_code == 200, response.text
+        session = Session()
 
-    response = client.post(
-        "http://127.0.0.1:8083/reserve", json={"train_id": train_id, "count": 4}
-    )
-requests.Session    assert response.status_code == 200, response.text
-    reservation = response.json()
-    assert reservation["train_id"] == "express_2000"
-    assert len(reservation["seats"]) == 4
-    assert reservation["seats"] == ["5A", "6A", "7A", "8A"]
-requests.Sessionrequests.Sessionrequests.Session
+        booking_reference = session.get("http://localhost:8082/booking_reference").text
+
+        train_data = session.get(
+            f"http://localhost:8081/data_for_train/" + train_id
+        ).json()
+        available_seats = (
+            s
+            for s in train_data["seats"].values()
+            if s["coach"] == "A" and not s["booking_reference"]
+        )
+        to_reserve = []
+        for i in range(seat_count):
+            to_reserve.append(next(available_seats))
+
+        seat_ids = [s["seat_number"] + s["coach"] for s in to_reserve]
+        reservation = {
+            "train_id": train_id,
+            "booking_reference": booking_reference,
+            "seats": seat_ids,
+        }
+
+        reservation_payload = {
+            "train_id": reservation["train_id"],
+            "seats": reservation["seats"],
+            "booking_reference": reservation["booking_reference"],
+        }
+
+        response = session.post(
+            "http://localhost:8081/reserve",
+            json=reservation_payload,
+        )
+        assert response.status_code == 200, response.text
+        response = response.json()
+
+        return json.dumps(reservation)
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True, port=8083)
